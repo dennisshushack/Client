a6822531b77276f998380a757c5614dda40781e3
dennisshushack <dennis.shushack@uzh.ch>
2021-03-08 22:30:48 +0100

    dsd
    sdsd

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -155,7 +155,7 @@ class Login extends React.Component {
 
             <ButtonContainer>
               <Button
-                disabled={!this.state.username || !this.state.name ||!this.state.password}
+                disabled={!this.state.username || !this.state.name || !this.state.password}
                 width="50%"
                 onClick={() => {
                   this.login();
